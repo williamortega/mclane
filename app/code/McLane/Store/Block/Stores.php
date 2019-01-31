28ddad063128879ffd7732083e080430e62a94a4
William Ortega <william.es@squeaky.com>
2019-01-31 16:52:54 -0600

    Updated URL of store to set SID

--- app/code/McLane/Store/Block/Stores.php
+++ app/code/McLane/Store/Block/Stores.php
@@ -3,6 +3,7 @@
 namespace McLane\Store\Block;
 
 
+use Magento\Customer\Model\Session;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\App\Http\Context;
 use Magento\Store\Model\StoreManagerInterface;
@@ -20,17 +21,24 @@ class Stores extends Template
     protected $_storeManager;
 
     /**
+     * @var Session
+     */
+    protected $_customerSession;
+
+    /**
      * @inheritDoc
      */
     public function __construct(
         Template\Context $context,
         Context $httpContext,
         StoreManagerInterface $storeManager,
+        Session $customerSession,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->httpContext = $httpContext;
         $this->_storeManager = $storeManager;
+        $this->_customerSession = $customerSession;
     }
 
     /**
@@ -55,14 +63,27 @@ class Stores extends Template
         $list = $this->_storeManager->getStores();
         foreach ($list as $store) {
             if ($store->getCode() != 'default') {
-                $stores[] = [
-                    'name' => $store->getName(),
-                    'code' => $store->getCode(),
-                    'url' => $store->getBaseUrl(),
-                ];
+                if ($store->getIsActive()) {
+                    $url = $store->getBaseUrl() . 'stores/store/switch?SID=' . $this->getSIDSession();
+                    $stores[] = [
+                        'name' => $store->getName(),
+                        'code' => $store->getCode(),
+                        'url' => $url,
+                    ];
+                }
             }
         }
 
         return $stores;
+    }
+
+    /**
+     * Gets SID of current user.
+     *
+     * @return string
+     */
+    public function getSIDSession()
+    {
+        return $this->_customerSession->getSessionId();
     }
 }
