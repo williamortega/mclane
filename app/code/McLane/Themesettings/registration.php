fc3a2957a343842c724914573fe9225578c2e887
William Ortega <william.es@squeaky.com>
2019-02-19 09:55:16 -0600

    Create a theme settings module

---
+++ app/code/McLane/Themesettings/registration.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Register "McLane_Themesettings" module.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'McLane_Themesettings',
+    __DIR__
+);
